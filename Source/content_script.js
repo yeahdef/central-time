81ef8f8c554729062211cd97d9532d77da26dba8
Joey Liechty <joey.liechty@unt.edu>
2014-12-16 15:42:42 -0600

    working out some parsing logic

--- Source/content_script.js
+++ Source/content_script.js
@@ -31,14 +31,16 @@ function walk(node)
 
 function handleText(textNode) 
 {
+	// look at the text
 	var v = textNode.nodeValue;
-	Pattern p = Pattern.compile('^(?:(?:([01]?\d|2[0-3]):)?([0-5]?\d):) ?(PST|MNT|EST|PT|MT|ET)$');
-	Matcher m = p.matcher(v);
-
 	// try to match on a date looking bit 
+	var m = v.search('^(?:(?:([01]?\d|2[0-3]):)?([0-5]?\d):)\\s??(?:am|pm|AM|PM)\\s??(?:PST|MNT|EST|PT|MT|ET)$');
+	// if it matches, parse it to datetime
 	if (m) {
-
-		v = v.replace(/\bThe Cloud\b/g, "My Butt");
+		var hour = v.split(':')[0].replace(/\W+/g, " ")
+		var minute = v.split(':')[1].replace(/\W+/g, " ")
+		var d = new Date(hour=hour, min=minute)
+		v = v.replace(/\bpm\b/g, "My Butt");
 	}
 
 	
