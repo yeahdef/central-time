b19055fee2be9aa65a3710f6d8594ad2bb349179
Joey Liechty <joey.liechty@unt.edu>
2014-12-16 15:57:31 -0600

    going to need a switch i suppose

--- Source/content_script.js
+++ Source/content_script.js
@@ -1,28 +1,24 @@
 walk(document.body);
-
 function walk(node) 
 {
 	// I stole this function from here:
 	// http://is.gd/mwZp7E
 	// I stole this function from here:
 	// https://github.com/panicsteve/cloud-to-butt/
-
 	var child, next;
-
-	switch ( node.nodeType )  
+	switch (node.nodeType)  
 	{
 		case 1:  // Element
 		case 9:  // Document
 		case 11: // Document fragment
 			child = node.firstChild;
-			while ( child ) 
+			while (child) 
 			{
 				next = child.nextSibling;
 				walk(child);
 				child = next;
 			}
 			break;
-
 		case 3: // Text node
 			handleText(node);
 			break;
@@ -38,11 +34,30 @@ function handleText(textNode)
 	// if it matches, parse it to datetime
 	if (m) {
 		var hour = v.split(':')[0].replace(/\W+/g, " ")
-		var minute = v.split(':')[1].replace(/\W+/g, " ")
-		var d = new Date(hour=hour, min=minute)
+		// var minute = v.split(':')[1].replace(/\W+/g, " ")
+		// var am = v.split(':')[1].replace(/\W+/g, " ")
+		// var tz = v.split(':')[1].replace(/\W+/g, " ")
+		// determine the offset
+		var offset = 0
+		switch (tz)  
+		{
+			case 'EST':
+				offset = -1
+			case 'ET':
+				offset = -1
+			case 'MNT':
+				offset = 1
+			case 'MT':
+				offset = 1
+			case 'PST':
+				offset = 2
+			case 'PT':
+				offset = 2
+		}
+		// apply the offset
+		hour = hour + offset
+		var d = new Date(hour=hour)
 		v = v.replace(/\bpm\b/g, "My Butt");
 	}
-
-	
 	textNode.nodeValue = v;
 }
